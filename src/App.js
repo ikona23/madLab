b447274550aa9709bf06d23586a7ad38e5d5237e
ikona23 <ikona23@gmail.com>
2016-11-16 03:23:05 +1300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header'
 import StorePicker from './components/StorePicker'
 
 
 class App extends Component {
   render() {
-    return <p>Hello</p>
+    return (
+      <div className="burgerOftheDay">
+      <div className="menu">
+        <Header />
+        <StorePicker />
+      </div>
+        <h1>Inventory</h1>
+      </div>
+    )
   }
 }
+
+
+
 
 
 export default App;
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
 
 
-class StorePicker extends Component{
+class StorePicker extends React.Component{
   render(){
   return (
     <form className="store-selector">
