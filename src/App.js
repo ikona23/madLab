f778c02f03ac476f87337296abc62d52ffb783f1
ikona23 <ikona23@gmail.com>
2016-11-16 02:33:49 +1300

    first component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
+import StorePicker from './components/StorePicker'
+
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welo</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <p>Hello</p>
   }
 }
+
 
 export default App;
---
+++ src/components/StorePicker.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+class StorePicker extends Component{
+  render(){
+  return (
+    <form className="store-selector">
+    <h2>Please enter a store</h2>
+    <input type="text" require placeholder="Store Name"/>
+    <button type="submit">Visit Store</button>
+    </form>
+  )
+  }
+}
+
+
+export default StorePicker
