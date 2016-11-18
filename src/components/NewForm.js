532457ddb596fe88ffd3401d31d02f67b6843f25
ikona23 <ikona23@gmail.com>
2016-11-18 23:20:07 +1300

    adding state

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -9,17 +9,23 @@ class NewForm extends React.Component {
     render() {
       return (
         <form className="editMenu" onSubmit={this.createMenuItem.bind(this)}>
+
             <input type='text' placeholder='Food Name' />
             <input type='text' placeholder='Food Price' />
+            <div>
             <select>
-              <option value="available">Fresh</option>
-              <option value="unavailable">Sold out!</option>
+              <option defaultValue="">Choose your option</option>
+              <option defaultValue="1">Option 1</option>
+              <option defaultValue="2">Option 2</option>
+              <option defaultValue="3">Option 3</option>
             </select>
+          </div>
             <textarea placeholder="Menu Desc"></textarea>
             <input type='text' placeholder='Status' />
             <input type='text' placeholder='Food Description' />
             <input type='text' placeholder='Food Image' />
-            <Button type='submit'>+Add Item</Button>
+
+            <Button className='btn waves-effect waves-light' name="action" type='submit'>+Add Item</Button>
         </form>
       )
     }
