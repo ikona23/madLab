f3542c3d3bccc866dc017f32128451f656bfebc9
ikona23 <ikona23@gmail.com>
2016-11-19 03:34:31 +1300

    add initial state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/Header'
-
+import StorePicker from './components/StorePicker'
 import Order from './components/Order'
 import Inventory from './components/Inventory'
 import { getNow } from './helpers'
 
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      burgers:{},
+      order:{}
+    }
+  }
   render() {
     return (
       <div className="burgerOftheDay">
