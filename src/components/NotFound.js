377cdc18bdd6e29428ce349fe0177b9f69afbac3
ikona23 <ikona23@gmail.com>
2016-11-22 23:53:39 +1300

    not found component

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,6 +4,9 @@ class NotFound extends React.Component {
   render() {
     return (
         <h2 className="notFound">Not found</h2>
+        <h3>404 page not found</h3>
+        <p>We are sorry but the page you are looking for does not exist.</p>
+
     )
   }
 }
