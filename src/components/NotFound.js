8045a99ae0bcd32fbfcda3bee299a09299857c1b
ikona23 <ikona23@gmail.com>
2016-11-22 23:56:10 +1300

    404 works

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,10 +3,10 @@ import React from 'react';
 class NotFound extends React.Component {
   render() {
     return (
-        <h2 className="notFound">Not found</h2>
+      <div>
         <h3>404 page not found</h3>
         <p>We are sorry but the page you are looking for does not exist.</p>
-
+      </div>
     )
   }
 }
